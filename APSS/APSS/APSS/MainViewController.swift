6a3f5e2aafd640d3a5c6a9c1cce29801205722db
OVNick <onnickvas@gmail.com>
2022-10-02 16:12:06 +0300

    Создал проект

---
+++ APSS/APSS/APSS/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  APSS
+//
+//  Created by Николай Онучин on 02.10.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
